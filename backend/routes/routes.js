c746177afc5302bf4f78fb19e2c3db429f70725d
LucaPetrescu <luca6011@gmail.com>
2023-07-28 03:49:12 +0300

    done x4 finally

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,21 +4,6 @@ const { postNewImages, getImages } = require("../controllers/controllers");
 const Drawings = require("../database/model");
 
 router.post("/postImages", async (req, res) => {
-  // const data = req.body;
-  // try {
-  //   if (data.editedImage === "undefined") {
-  //     data.editedImage = "";
-  //   }
-  //   const imagesId = data.imagesId;
-  //   const originalImage = data.originalImageInBase64;
-  //   const editedImage = data.editedImage;
-  //   const newDrawing = new Drawings({ imagesId, originalImage, editedImage });
-  //   await newDrawing.save();
-  //   res.status(200).send("Image successfuly created");
-  // } catch (error) {
-  //   console.log(error.message);
-  //   res.status(500).send("Error:", error.message);
-  // }
   const data = req.body;
   try {
     await postNewImages(data);
@@ -30,18 +15,6 @@ router.post("/postImages", async (req, res) => {
 });
 
 router.get("/getImages/:imagesId", async (req, res) => {
-  // try {
-  //   const imagesId = req.params.imagesId;
-  //   const images = await Drawings.find({ imagesId });
-  //   if (images.length === 0) {
-  //     throw new Error("No documents with that id found");
-  //   }
-  //   console.log(images);
-  //   res.status(200).send(images);
-  // } catch (error) {
-  //   console.log(error.message);
-  //   res.status(500).send(error);
-  // }
   const imagesId = req.params.imagesId;
   try {
     const images = await getImages(imagesId);
