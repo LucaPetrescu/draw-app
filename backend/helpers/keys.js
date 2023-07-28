2d0fc23399f02ca2be0a7773082ebf9e5bd98509
LucaPetrescu <45922759+LucaPetrescu@users.noreply.github.com>
2023-07-28 03:52:14 +0300

    minor tweaks

--- backend/helpers/keys.js
+++ backend/helpers/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Luca:normandysr2@cluster0.ktaeq.mongodb.net/Drawings?retryWrites=true&w=majority",
+    "mongodb+srv://Luca:<yourpassword>@cluster0.ktaeq.mongodb.net/Drawings?retryWrites=true&w=majority",
 };
